5670f37527b1ba1e9892b73ee644ea72ba4d61f2
Mohnish Thallavajhula <mohnish@billfloat.com>
2012-06-23 19:40:09 -0700

    the bot is now much more smarter

--- poker.js
+++ poker.js
@@ -1,8 +1,9 @@
 // Setting up everything we need to attack the pokers ;-)
 var listOfPokers = document.querySelectorAll('a[ajaxify^="/ajax/pokes/poke_inline.php?uid="]')
   , poke = document.createEvent('MouseEvents')
-  , mouseOverPoker = document.createEvent('MouseEvents')
-  , pokeDialog = document.querySelector('.fbRemindersStory img[alt=Pokes]');
+  , mouseOverPoker = document.createEvent('MouseEvents');
+  , pokeDialog = document.querySelector('.fbRemindersStory img[alt=Pokes]')
+  , pageHasPokeDialog = !!pokeDialog;
 
 // Setting up the poke itself
 poke.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
@@ -10,8 +11,12 @@ poke.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, fa
 // Fooling FB into thinking that it's actually done by a human and not a bot
 mouseOverPoker.initMouseEvent('mouseover', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
 
-// The bot opens the list of pokers dialog
-pokeDialog.dispatchEvent(poke);
+// The bot opens the list of pokers dialog. If the dialog is not present, visit the pokes page.
+if(pageHasPokeDialog) {
+  pokeDialog.dispatchEvent(poke);
+} else {
+  window.location = '/pokes';
+}
 
 // Missiles away...poke-em-all!!!!111
 Array.prototype.forEach.call(listOfPokers, function(poker) {
@@ -21,5 +26,9 @@ Array.prototype.forEach.call(listOfPokers, function(poker) {
   }, 1000);
 });
 
-// The bot closes the list of pokers dialog
-pokeDialog.dispatchEvent(poke);
+// The bot closes the list of pokers dialog if it exists. If it doesn't, it just takes you back to the homepage.
+if(pageHasPokeDialog) {
+  pokeDialog.dispatchEvent(poke);
+} else {
+  window.location = '/';
+}
